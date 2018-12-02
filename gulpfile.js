685e1a5d7bf1da34f06c0760b3f62e3ac8e44817
wxl <1341206737@qq.com>
2018-12-02 18:45:56 +0800

    swiper已做完

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,47 @@
+/*
+ * @Author: 王鑫磊 
+ * @Date: 2018-12-01 10:46:43 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-12-01 11:35:38
+ */
+
 
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+
+var server = require('gulp-webserver');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var data = require('./src/json/index.json')
+
+
+gulp.task('server',function(){
+    return gulp.src('src')
+    .pipe(server({
+        port:8888,
+        open:true,
+        middleware:function(req,res,next){
+
+
+            var pathname = url.parse(req.url).pathname;
+            if(pathname === '/favicon.ico'){
+                res.end('')
+                return; 
+            }
+
+            if(pathname === '/getData'){
+                res.end(JSON.stringify({code:1,list:data[0]}))
+            }else{
+                pathname = pathname === '/' ? "index.html" : pathname;
+                res.end(fs.readFileSync(path.join(__dirname,'src',pathname)))
+            }
+           
+        }
+    }))
+})
+
 
 gulp.task('devScss',function(){
     return gulp.src('./src/scss/index.scss')
@@ -12,3 +52,5 @@ gulp.task('devScss',function(){
 gulp.task('watch',function(){
     return gulp.watch('./src/scss/index.scss',gulp.series('devScss'))
 })
+
+gulp.task('dev',gulp.parallel('devScss','server','watch'))
---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: 王鑫磊 
+ * @Date: 2018-12-01 11:15:08 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-12-02 18:42:24
+ */
+
+
+
+$.ajax({
+    url:'/getData',
+    dataType:"json",
+    success:function(res){
+        xuanran(res.list)
+    }
+})
+
+function xuanran(data){
+
+    console.log(data)
+
+    for(var i in data){
+        var html = ``;
+        html+=`<div class="swiper-slide">`
+
+        data[i].forEach(function(file){
+            html+= `<dl>
+            <dt> <img src="${file.url}" alt=""></dt>
+            <dd>${file.title}</dd>
+            </dl>`
+        })
+           
+        html+=`</div>`
+        $('.swiper-wrapper').append(html);
+        new Swiper('.swiper-container')
+    }
+
+    
+
+
+    
+}
+
